1cb54fcb8b824537af396b910b12d05209fbc6ad
Efface Studios <33323258+effacestudios@users.noreply.github.com>
2018-11-21 16:11:50 +0500

    Download

---
+++ download.cs
@@ -0,0 +1,15 @@
+using System;
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine(@"Download Syn Bot SIML Bot Unity 3D From Here: https://www.effacestudios.com/shop/unity-siml-bot-synbot/");
+            Console.ReadKey();
+        }
+    }
+}
